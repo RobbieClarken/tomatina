c7a5af09621ee01c68e1942c27ea928462395993
RobbieClarken <robbie.clarken@gmail.com>
2019-10-27 17:30:04 +1100

    Create named pipe for detecting button press

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
 #![allow(dead_code)]
 extern crate libusb;
+use libc::mkfifo;
 use libusb::{Context, DeviceHandle};
+use std::ffi::CString;
+use std::fs;
+use std::io::Read;
+use std::io::Result;
+use std::os::unix::fs::OpenOptionsExt;
+use std::path::Path;
+use std::thread::sleep;
 use std::time::Duration;
 
-const VENDOR_ID: u16 = 0xD209;
-const PRODUCT_ID: u16 = 0x1200;
+const USBBTN_VENDOR_ID: u16 = 0xD209;
+const USBBTN_PRODUCT_ID: u16 = 0x1200;
 const USBBTN_INTERFACE: u16 = 0;
 const USBBTN_VALUE: u16 = 0x0200;
-// const USBBTN_MESG_LENGTH = 4;
-// const USBBTN_SIZE = 64;
 const UM_REQUEST_TYPE: u8 = 0x21;
 const UM_REQUEST: u8 = 9;
-const UM_TIMEOUT: Duration = Duration::from_millis(2000);
+const TIMEOUT: Duration = Duration::from_millis(2000);
+
+const WORKING: Color = Color {
+    red: 0,
+    green: 0,
+    blue: 255,
+};
+const BREAK: Color = Color {
+    red: 0,
+    green: 255,
+    blue: 0,
+};
+const CHANGE: Color = Color {
+    red: 255,
+    green: 0,
+    blue: 0,
+};
 
 enum ButtonAction {
     Alternate = 0,
@@ -34,7 +56,7 @@ struct Color {
 }
 
 fn open_device(context: &Context) -> Option<DeviceHandle> {
-    context.open_device_with_vid_pid(VENDOR_ID, PRODUCT_ID)
+    context.open_device_with_vid_pid(USBBTN_VENDOR_ID, USBBTN_PRODUCT_ID)
 }
 
 fn set_button_data(handle: &DeviceHandle, released_color: &Color, pressed_color: &Color) {
@@ -73,27 +95,49 @@ fn set_button_data(handle: &DeviceHandle, released_color: &Color, pressed_color:
                 USBBTN_VALUE,
                 USBBTN_INTERFACE,
                 &buf[offset..offset + 4],
-                UM_TIMEOUT,
+                TIMEOUT,
             )
             .expect("transfer failed");
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let mut context = libusb::Context::new().unwrap();
     context.set_log_level(libusb::LogLevel::Debug);
     let handle = open_device(&context).expect("failed to find USBButton");
     set_button_data(
         &handle,
+        &WORKING,
         &Color {
             red: 255,
-            green: 0,
-            blue: 255,
-        },
-        &Color {
-            red: 255,
-            green: 0,
+            green: 255,
             blue: 255,
         },
     );
+    let p = Path::new("/tmp/tomatina.fifo");
+    create_named_pipe(p)?;
+    let mut file = fs::OpenOptions::new()
+        .read(true)
+        .custom_flags(libc::O_NONBLOCK)
+        .open(p)?;
+    let mut buf: [u8; 1] = [0; 1];
+    loop {
+        let read = file.read(&mut buf)?;
+        if read > 0 {
+            if buf[0] as char == '1' {
+                println!("transitioning...");
+            }
+        }
+        sleep(Duration::from_millis(50));
+    }
+}
+
+fn create_named_pipe(p: &Path) -> Result<()> {
+    if p.exists() {
+        fs::remove_file(p)?;
+    }
+    let path = CString::new(p.to_str().unwrap())?;
+    let result: i32 = unsafe { mkfifo(path.as_ptr(), 0o600) }.into();
+    assert_eq!(result, 0);
+    Ok(())
 }
